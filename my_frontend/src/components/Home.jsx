6e9eaadd141bc2001f8b18a3b7d8c05adb3bba5c
shushant kumar <shushantkumar@192.168.1.5>
2024-12-19 20:40:46 +0530

    GitHub Repo: https://github.com/adityakarn06/Restaurant-App.git

---
+++ my_frontend/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='flex items-center justify-center h-screen'>
+    <h1 className='text-center text-3xl'>Welcome to Restaurant App</h1>
+  </div>
+  )
+}
+
+export default Home
